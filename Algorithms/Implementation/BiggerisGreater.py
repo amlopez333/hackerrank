510e06b694abac10e5534601d1f7971c31a3fdf6
Asistente <Asistente>
2016-10-11 10:10:15 -0600

    Passed all cases

--- Algorithms/Implementation/BiggerisGreater.py
+++ Algorithms/Implementation/BiggerisGreater.py
@@ -1,42 +1,43 @@
-import itertools
 def getT():
     t = int(input().strip())
     return t
 def getW():
-    w = input().strip()
+    w = list(input().strip())
     return w
-def getIterator(w):
-    return iter(itertools.permutations(w))
-def getNext(iterator):
-    return next(iterator)
-def findNextBigger(w):
-    iterator = getIterator(w)
-    current = ()
-    if(w.count(w[0])/len(w) == 1):
+def getNonIncreasingIndex(w):
+    for i in range(len(w)-1, -1, -1):
+        if w[i-1]<w[i]:
+            return i
+    return i
+def getPivot(w,i):
+    return i-1
+def getNextAfterPivot(w, i, pivot):
+    for j in range(len(w)-1, -1, -1 ):
+        if(w[j]>w[pivot]):
+            return j
+    return indexOfNextGreatest
+def swap(w, j, pivot):
+    w[j], w[pivot] = w[pivot], w[j]
+    return w
+def reverseSuffix(w,i):
+    w = w[:i] + w[len(w) - 1 : i - 1 : -1]
+    return w
+def buildString(w):
+    return ''.join(w)
+def getNextPermutation(w):
+    nonIncreasingIndex = getNonIncreasingIndex(w)
+    if(nonIncreasingIndex <= 0):
         return "no answer"
-    try:
-        while(current != None):
-            current = getNext(iterator)
-            if(current > tuple(w)):
-                break
-    except:
-        return("no answer")
-    try:
-        while(getNext(iterator) != None):
-           
-            if(getNext(iterator) < current and getNext(iterator()> tuple(w))):
-                
-                current = getNext(iterator)
-            
-            
-        return(''.join(current))
-    except:   
-        return(''.join(current))
+    pivot = getPivot(w, nonIncreasingIndex)
+    nextAfterPivot = getNextAfterPivot(w, nonIncreasingIndex, pivot)
+    w = swap(w, nextAfterPivot, pivot)
+    w = reverseSuffix(w, nonIncreasingIndex)
+    return buildString(w)
 def main():
     t = getT()
     for i in range(t):
         w = getW()
-        print(findNextBigger(w))
+        print(getNextPermutation(w))
 
 main()
     
